db9b908437969e28c3131e68cc2a3f85b9fec045
navneet-chugh <navneetchugh15@gmail.com>
2023-08-18 21:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Ask for confirmation
+var confirmation = confirm("Do you want to proceed?");
+
+// Check user's choice and display appropriate alert
+if (confirmation) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
